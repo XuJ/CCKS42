44f143bea742e2d0d4730be255e5464aaf9772b4
hujiaxin <hujiaxin>
2020-08-12 17:13:41 +0800

    fix error

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,7 +1,7 @@
 import re
 
 
-def argument_cleaning(content):
+def argument_cleaning_v1(content):
   text = re.sub(r'\s', ' ', content)
   text = re.sub(r'<br>', ' ', text)
   text = re.sub(r'&nbsp', ' ', text)
@@ -10,4 +10,17 @@ def argument_cleaning(content):
   for d in matched_dates:
     words_blank = " " + d + " "
     text = text.replace(d, words_blank)
+  return text
+
+
+def argument_cleaning(content):
+  text = re.sub(r'\s', ' ', content)
+  text = re.sub(r'<br>', ' ', text)
+  text = re.sub(r'&nbsp', ' ', text)
+  pattern = r'\d{4}[-|年|./]+\d{1,2}[-|月|./]+\d{1,2}[日]?'
+  matched_dates = re.finditer(pattern, text)
+  for i, d in enumerate(matched_dates):
+      s, e = d.span()
+      words_blank = " " + d.group() + " "
+      text = text[:(i*2)+s] + words_blank + text[(i*2)+e:]
   return text
