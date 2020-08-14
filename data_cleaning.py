8558196b9892264c48a2e49ff9ba5beda808d0a7
jiaoxu <jiaoxu@myhexin.com>
2020-08-14 13:55:03 +0800

    update data prepare annotation

--- data_cleaning.py
+++ data_cleaning.py
@@ -3,13 +3,12 @@ import re
 
 def argument_cleaning_v1(content):
   text = re.sub(r'\s', ' ', content)
-  text = re.sub(r'<br>', ' ', text)
-  text = re.sub(r'&nbsp', ' ', text)
   pattern = r'\d{4}[-|年|./]+\d{1,2}[-|月|./]+\d{1,2}[日]?'
   matched_dates = re.findall(pattern, text)
   for d in matched_dates:
     words_blank = " " + d + " "
     text = text.replace(d, words_blank)
+  text = re.sub(r'\s', ' ', text)
   return text
 
 
--- data_prepare_annotation.py
+++ data_prepare_annotation.py
@@ -1,13 +1,12 @@
 import os
 import json
 
-import re
-import random
+from reannotate_training_data import ReAnnotate
+from data_cleaning import argument_cleaning_v1
 
-model_name = 'electra_large'
 data_dir = 'data\\ccks 4_2 Data'
 input_file = 'event_element_train_data_label.txt'
-output_dir = 'data\\analysis'
+output_dir = 'data\\annotation'
 role_event_type_dict = {
   '高层死亡': ['公司名称', '高层人员', '高层职务', '死亡/失联时间', '死亡年龄'],
   '破产清算': ['公告时间', '公司名称', '公司行业', '受理法院', '裁定时间'],
@@ -19,6 +18,9 @@ role_event_type_dict = {
   '股东增持': ['增持金额', '增持的股东', '增持开始日期'],
   '股东减持': ['减持金额', '减持的股东', '减持开始日期'],
 }
+event_type_idx_dict = {}
+for k in role_event_type_dict.keys():
+  event_type_idx_dict[k] = 0
 event_type_questions_dict = {}
 for k, v in role_event_type_dict.items():
   event_type_questions_dict[k] = []
@@ -31,45 +33,53 @@ for k, v in role_event_type_dict.items():
       'key': i
     })
 
-random.seed(42)
-idx = random.randint(1, 100)
 with open(os.path.join(data_dir, input_file), 'r', encoding='utf8') as input_fh:
   for i, org_line in enumerate(input_fh):
     org_json = json.loads(org_line)
     org_text = org_json['content']
-    text = re.sub(r'\s', ' ', org_text)
+    doc_id = org_json['doc_id']
+    text = argument_cleaning_v1(org_text)
+    re_annotate = ReAnnotate(text)
 
     qas = []
     for question_main_idx, event in enumerate(org_json['events']):
       qas_sub = []
       event_type = event['event_type']
       role_list = role_event_type_dict[event_type]
-      annotated_role_set = set(event.keys()) - set(['event_type', 'event_id'])
-      assert annotated_role_set.issubset(set(role_list))
-
       questions = event_type_questions_dict[event_type]
+      arguments = []
+      roles = []
+      for role in event.keys():
+        if role not in ['event_type', 'event_id']:
+          argument = event[role].strip()
+          if len(argument) > 0:
+            loc = text.find(argument)
+            if re_annotate.digit_check(argument, loc):
+              arguments.append(argument)
+              roles.append(role)
+
+      if len(roles) == 0:
+        continue
+      argument_locs = re_annotate.generate_locs(arguments)
+      argument_loc_dict = {}
+      for l, a in zip(argument_locs, arguments):
+        argument_loc_dict[a] = l
+
       for question_minor_idx, role in enumerate(role_list):
-        if role in event.keys():
-          argument = event[role]
-          argument_start = text.find(argument)
-          if argument_start == -1:
-            print(argument, role, text)
-            qas_sub.append({
-              'question': role,
-              'answers': []
-            })
-          else:
-            qas_sub.append({
-              'question': role,
-              'answers': [{
-                'start_block': '0',
-                'start': argument_start,
-                'end_block': '0',
-                'end': argument_start + len(argument) - 1,
-                'text': argument,
-                'sub_answer': None
-              }]
-            })
+        if role in roles:
+          argument = event[role].strip()
+          argument_start = argument_loc_dict[argument]
+          qas_sub.append({
+            'question': role,
+            'answers': [{
+              'start_block': '0',
+              'start': argument_start,
+              'end_block': '0',
+              'end': argument_start + len(argument) - 1,
+              'text': argument,
+              'sub_answer': None
+            }]
+          })
         else:
           qas_sub.append({
             'question': role,
@@ -84,16 +94,16 @@ with open(os.path.join(data_dir, input_file), 'r', encoding='utf8') as input_fh:
           'text': text,
         }
       ],
-      'key': '{}'.format(i),
+      'key': doc_id,
       'qas': qas,
       'questions': questions,
       'status': 1,
     }
-    output_file = '{}_{}.json'.format(event_type, idx)
+    output_file = '{}_{}.json'.format(event_type, event_type_idx_dict[event_type])
     if os.path.exists(os.path.join(output_dir, output_file)):
       if os.stat(os.path.join(output_dir, output_file)).st_size >= 900000:
-        idx = random.randint(1, 100)
-        output_file = '{}_{}.json'.format(event_type, idx)
+        event_type_idx_dict[event_type] = event_type_idx_dict.get(event_type)+1
+        output_file = '{}_{}.json'.format(event_type, event_type_idx_dict[event_type])
     with open(os.path.join(output_dir, output_file), 'a', encoding='utf8') as output_fh:
       output_line = json.dumps(data, ensure_ascii=False)
       output_fh.write(output_line)
