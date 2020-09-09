7194cd284616571841e70ca05b448a73953fe22c
jiaoxu <jiaoxu@myhexin.com>
2020-09-09 09:40:08 +0800

    add date/name_reformat in post_processing

--- post_processing.py
+++ post_processing.py
@@ -7,7 +7,70 @@ result_file = 'electra_annot_finetuning_data_output_electra_large_result_2020090
 output_file = '{}_single.txt'.format(result_file.split('.')[0])
 output_file2 = '{}_full.txt'.format(result_file.split('.')[0])
 abbr_file = 'data\\abbr_map.json'
+pred_file = 'data\\ccks 4_2 Data\\event_element_dev_data.txt'
 
+doc_id_text_dict = {}
+with open(pred_file, 'r', encoding='utf8') as pred_fh:
+  for org_line in pred_fh:
+    data = json.loads(org_line)
+    doc_id_text_dict[data['doc_id']] = data['content']
+
+
+def reformat_date(d, doc_id):
+  text = doc_id_text_dict[doc_id]
+  year_flag = int('年' in d)
+  month_flag = int('月' in d)
+  day_flag = int('日' in d)
+  if 3 > year_flag + month_flag + day_flag > 0:
+    if year_flag and month_flag:
+      new_d = d + '日'
+      if text.find(new_d) >= 0:
+        print('add 日:', doc_id, d, new_d)
+        return new_d
+      else:
+        return ''
+    elif year_flag and month_flag:
+      print('我也不知道这种情况怎么改', d)
+      return ''
+    elif month_flag and day_flag:
+      if d.startswith('-'):
+        d = d[1:]
+      if text.find('2019年' + d) >= 0:
+        new_d = '2019年' + d
+        print('add 2019年:', doc_id, d, new_d)
+        return new_d
+      elif text.find('2020年' + d) >= 0:
+        new_d = '2020年' + d
+        print('add 2020年:', doc_id, d, new_d)
+        return new_d
+      else:
+        return ''
+    else:
+      return ''
+  return d
+
+
+def reformat_name(n, doc_id):
+  text = doc_id_text_dict[doc_id]
+  if len(n) > 2:
+    return n
+  elif len(n) == 2:
+    if text.find(n) >= 0:
+      return n
+    else:
+      print(doc_id, n)
+      return ''
+  else:
+    print(n)
+    return ''
+
+
+role_entity_type_dict = {
+  '公司名称': '公司', '高层人员': '人名', '高层职务': '职称', '死亡/失联时间': '时间', '死亡年龄': '数字', '公司行业': '行业', '公告时间': '时间', '受理法院': '机构',
+  '裁定时间': '时间', '损失金额': '数字', '其他损失': '文本短语', '赔付金额': '数字', '赔付对象': '公司/人名', '伤亡人数': '数字', '其他影响': '文本短语', '冻结金额': '数字',
+  '被冻结股东': '公司/人名', '冻结开始日期': '时间', '冻结结束日期': '时间', '质押金额': '数字', '质押方': '公司/人名', '接收方': '公司/人名', '质押开始日期': '时间',
+  '质押结束日期': '时间', '增持金额': '数字&单位', '增持的股东': '公司/人名', '增持开始日期': '时间', '减持金额': '数字&单位', '减持的股东': '公司/人名', '减持开始日期': '时间',
+}
 single_event_types = ['高层死亡', '重大安全事故', '重大资产损失', '破产清算', '重大对外赔付']
 with open(abbr_file, 'r', encoding='utf8') as abbr_fh:
   abbr_maps = json.load(abbr_fh)
@@ -41,6 +104,14 @@ with open(os.path.join(data_dir, result_file), 'r', encoding='utf8') as input_fh
           v = abbr_map[v]
         # if v != event[k]:
         #   print(v, event[k])
+        if k != 'event_type' and role_entity_type_dict[k] == '时间':
+          v = reformat_date(v, doc_id)
+        if k != 'event_type' and '人名' in role_entity_type_dict[k]:
+          v = reformat_name(v, doc_id)
+        if k != 'event_type' and '公司' in role_entity_type_dict[k]:
+          v = reformat_name(v, doc_id)
+        if v == '':
+          continue
         event[k] = v
         event_str += '{}_{}/'.format(k, v)
       if event_str not in events_set:
