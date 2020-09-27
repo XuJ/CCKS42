67f5589b22b5e7b162fe7f9999f69629d14183bf
jiaoxu <jiaoxu@myhexin.com>
2020-09-27 16:45:05 +0800

    add data prepare merge train&dev

---
+++ data_prepare_merge_train_dev.py
@@ -0,0 +1,41 @@
+import argparse
+import json
+import os
+from collections import deque
+
+import tensorflow.compat.v1 as tf
+
+
+def run_data_prepare(data_dir, part):
+  input_dir = os.path.join(data_dir, 'electra_ensemble_{}'.format(part), 'finetuning_data', part)
+
+  tf.io.gfile.copy(os.path.join(input_dir, 'train.json'), os.path.join(input_dir, 'train_backup.json'), True)
+  tf.io.gfile.copy(os.path.join(input_dir, 'dev.json'), os.path.join(input_dir, 'dev_backup.json'), True)
+  with tf.io.gfile.GFile(os.path.join(input_dir, 'train_backup.json'), 'r') as input_fh1, tf.io.gfile.GFile(
+      os.path.join(input_dir, 'dev_backup.json'), 'r') as input_fh2, tf.io.gfile.GFile(
+      os.path.join(input_dir, 'train.json'), 'w') as output_fh:
+    orig_json1 = json.load(input_fh1)
+    orig_json2 = json.load(input_fh2)
+    train_json = {
+      'version': 'v2.0', 'data': []
+    }
+    for data in orig_json1['data']:
+      train_json['data'].append(data)
+    for data in orig_json2['data']:
+      train_json['data'].append(data)
+    json.dump(train_json, output_fh, ensure_ascii=False)
+  tf.io.gfile.remove(os.path.join(input_dir, 'train_backup.json'))
+  tf.io.gfile.remove(os.path.join(input_dir, 'dev_backup.json'))
+  print('Done')
+
+
+def main():
+  parser = argparse.ArgumentParser()
+  parser.add_argument("--dir", required=True, help="location of all data")
+  parser.add_argument("--part", default='full', help="build full/single/multi event model")
+  args = parser.parse_args()
+  run_data_prepare(args.dir, args.part)
+
+
+if __name__ == '__main__':
+  main()
